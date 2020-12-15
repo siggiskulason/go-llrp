521a5476889f84c6b61a7847f0787cbaa7f07633
Siggi Skulason <siggi.skulason@canonical.com>
2020-12-15 18:33:15 +0000

    local changes

--- llrp.go
+++ llrp.go
@@ -13,26 +13,28 @@ import (
 // LLRP header values, the header is composed of 2 bytes where the first
 // is set to 0x40 or 1024, so 1025 would be 0x0401
 const (
-	GetReaderCapabilityHeader         = 1025 // type 1
-	GetReaderConfigHeader             = 1026 // type 2
-	SetReaderConfigHeader             = 1027 // type 3
-	GetReaderCapabilityResponseHeader = 1035 // type 11
-	GetReaderConfigResponseHeader     = 1036 // type 12
-	SetReaderConfigResponseHeader     = 1037 // type 13
-	AddRospecHeader                   = 1044 // type 20
-	DeleteRospecHeader                = 1045 // type 21
-	EnableRospecHeader                = 1048 // type 24
-	AddRospecResponseHeader           = 1054 // type 30
-	DeleteRospecResponseHeader        = 1055 // type 31
-	EnableRospecResponseHeader        = 1058 // type 34
-	DeleteAccessSpecHeader            = 1065 // type 41
-	DeleteAccessSpecResponseHeader    = 1075 // type 51
-	ROAccessReportHeader              = 1085 // type 61
-	KeepaliveHeader                   = 1086 // type 62
-	ReaderEventNotificationHeader     = 1087 // type 63
-	EventsAndReportsHeader            = 1088 // type 64
-	KeepaliveAckHeader                = 1096 // type 72
-	ImpinjEnableCutomMessageHeader    = 2047 // type 1023
+	GetReaderCapabilityHeader         = 1025      // type 1
+	GetReaderConfigHeader             = 1026      // type 2
+	SetReaderConfigHeader             = 1027      // type 3
+	GetReaderCapabilityResponseHeader = 1035      // type 11
+	GetReaderConfigResponseHeader     = 1036      // type 12
+	SetReaderConfigResponseHeader     = 1037      // type 13
+	AddRospecHeader                   = 1044      // type 20
+	DeleteRospecHeader                = 1045      // type 21
+	EnableRospecHeader                = 1048      // type 24
+	AddRospecResponseHeader           = 1054      // type 30
+	DeleteRospecResponseHeader        = 1055      // type 31
+	EnableRospecResponseHeader        = 1058      // type 34
+	DeleteAccessSpecHeader            = 1065      // type 41
+	MsgGetSupportedVersion            = 1024 + 46 // MessageType(46)
+	MsgGetSupportedVersionResponse    = 1024 + 56 //MessageType(56)
+	DeleteAccessSpecResponseHeader    = 1075      // type 51
+	ROAccessReportHeader              = 1085      // type 61
+	KeepaliveHeader                   = 1086      // type 62
+	ReaderEventNotificationHeader     = 1087      // type 63
+	EventsAndReportsHeader            = 1088      // type 64
+	KeepaliveAckHeader                = 1096      // type 72
+	ImpinjEnableCutomMessageHeader    = 2047      // type 1023
 )
 
 // Pack the data into (partial) LLRP packet payload.
